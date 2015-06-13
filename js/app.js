e48425125a72404dd233213ea5b4d94fb95ce145
Ethan <ewalker15@yahoo.com>
2015-06-13 13:06:47 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    /*Variable Declarations*/
+    var userGuess = 1;
+    var guessCount = 0;
+    var magicNumber = Math.floor((Math.random() * 100) + 1);
+    console.log(magicNumber); // Output magic number to console for testing
+
 });
 
 
